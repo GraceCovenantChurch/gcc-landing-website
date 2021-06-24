a2a260bcaf3924750d7148dca1e40b2d82141686
Joey Zia <joey.zia@gmail.com>
2021-06-24 14:18:11 -0400

    20210624 - jzia - Redesigned Page
    Redesigned the page to fit mobile pages. There are a few things I wished I knew better how to do but I'm stubborn and am not exactly pleased with using "vw" and "vh". I also don't want to use media queries. Seems like too much work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,95 @@
 import React from "react";
 
+import { ArrowRight } from "react-feather";
+
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
 
 import styles from "./App.module.scss";
 
-import background from "static/philadelphia.png";
-import logo from "static/logo.png";
-import gcctext from "static/gracecovenanttext_big.png";
+import logo from "static/gracecovenantchurchlogo.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background: "transparent",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+  },
+  buttons: {
+    color: theme.palette.getContrastText(grey[500]),
+    backgroundColor: grey[300],
+    "&:hover": {
+      backgroundColor: grey[500],
+    },
+    opacity: 0.675,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div
-      className={styles.App}
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <div className={styles.AppContent}>
-        <div className={`${styles.fadeIn} ${styles.header}`}>
-          <img className={styles.logo} alt="logo" src={logo} />
-        </div>
-        <Grid container className={styles.container}>
+    <div className={styles.App}>
+      <div className={styles.layer} />
+      <div className={`${styles.AppContent} ${classes.root}`}>
+        <Grid
+          container
+          alignItems="center"
+          direction="column"
+          justify="center"
+          spacing={3}
+        >
           <Grid item xs={12}>
-            <Grid container justify="center">
-              <Grid key={1} className={styles.left} item>
-                <h1 className={styles.delayedFadeIn}>
-                  <a href="http://ucity.gracecovenant.net">University City Site</a>
-                </h1>
-              </Grid>
-              <Grid key={2} className={styles.right} item>
-                <h1 className={styles.delayedFadeIn}>
-                  <a href="https://ml.gracecovenant.net">Main Line Site</a>
-                </h1>
-              </Grid>
-            </Grid>
+            <img className={styles.logo} alt="logo" src={logo} />
+          </Grid>
+          <Grid item xs={12}>
+            <p className={styles.title}>Grace Covenant Church</p>
+          </Grid>
+          <Grid item xs={12}>
+            <p className={styles.missionStatement}>
+              Raising up kingdom workers who are transformed by Christ to
+              influence the world.
+            </p>
+          </Grid>
+          <Grid item xs={12}>
+            <hr
+              style={{
+                color: "white",
+                backgroundColor: "white",
+                height: ".01vw",
+                width: "50vw",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              className={classes.buttons}
+              style={{ fontSize: "1.5vw" }}
+              color="primary"
+              size="large"
+              href="https://ml.gracecovenant.net"
+            >
+              <p className={styles.linkText}>Main Line</p>
+              <ArrowRight color="black" size="1.5vw" />
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              className={classes.buttons}
+              style={{ fontSize: "1.5vw" }}
+              color="primary"
+              size="large"
+              href="http://ucity.gracecovenant.net"
+            >
+              <p className={styles.linkText}>University City</p>
+              <ArrowRight color="black" size="1.5vw" />
+            </Button>
           </Grid>
         </Grid>
-        <div className={`${styles.footer} ${styles.fadeIn}`}>
-          <img className={styles.gccText} alt="logo" src={gcctext} />
-        </div>
-        <div className={`${styles.missionStatement} ${styles.delayedFadeIn}`}>
-          <i>Raising up kingdom workers who are transformed by Christ to influence the world.</i>
-        </div>
       </div>
     </div>
   );
